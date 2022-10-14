9729f9b6bc996befadc834fd6b4e33acda25d44f
Gabriel Rosales <gabrielalejandrorosalesazurdia@gmail.com>
2022-10-14 09:28:30 -0600

    agregando comentarios, cambiando de lugar funciones y dando funcionalidad a igual()

--- src/context/OperationContext.jsx
+++ src/context/OperationContext.jsx
@@ -2,93 +2,112 @@ import { createContext, useState, useEffect } from "react";
 
 export const OperationContext = createContext();
 
-// IDEA: cambiar el displaynumber de string a un array que siemper maneje datos
-// de esa manera para facilitar el manejo de weas
-
 export function OperationContextProvider(props) {
+	// Lista de numeros a utilizar en los botones
 	const numerosAUsar = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+	// Lista de simbolos a utilizar en los botones
 	const simbolosAUsar = ["+", "-", "/", "*", "="];
+	// Operación a realizar ahora
 	const [currentSymbol, setcurrentSymbol] = useState("");
+	// Elementos en la memoria de la calculadora
 	const [displayList, setdisplayList] = useState([0]);
 
-	//Encuentra los datos antes del simbolo y después el símbolo
+	//Encuentra los datos antes del simbolo y después el símbolo de la operación
+	// Devuelve la primera y segunda parte dentro de una array
 	function encontrarValores() {
 		const indexOfSymbol = displayList.indexOf(currentSymbol);
 		const firstpart = displayList.splice("", indexOfSymbol);
-		// Filtar falla cuando es la suma del mismo numero 2+2
-		// falla con !firstpart.includes(e)
 		const secondpart = displayList.filter((e) => {
 			return e != currentSymbol;
 		});
 		return [firstpart[0], secondpart[0]];
 	}
 
-	function actualizarDisplay(valorNuevo) {
-		const ultimoValor = displayList[displayList.length - 1];
-		const checkIfDefault = (displayList[0] == 0 && displayList.length == 1);
-		console.log("display list ", displayList);
+	// recibe dos números y devuelve su suma
+	function sumar(valor1, valor2) {
+		return valor1 + valor2;
+	}
 
-		// Cuando preciona una operación y ya hay una en display
-		// o preciona el simbolo igual
+	// recibe dos números y devuelve su resta
+	function restar(valor1, valor2) {
+		return valor1 - valor2;
+	}
+
+	// recibe dos números y devuelve su división
+	function dividir(valor1, valor2) {
+		console.log("dividiendo");
+	}
+
+	// recibe dos números y devuelve su multiplicación
+	function multiplicar(valor1, valor2) {
+		console.log("multiplicando");
+	}
+
+	// función encargada de dar el resultado de una operación
+	function igual() {
+		// Encuentra los valores antes y después del simbolo de la operacion
+		const valores = encontrarValores();
+		// Si es suma llama a suma y suma los números
+		if (currentSymbol == "+") {
+			return sumar(valores[0], valores[1]);
+		}
+		// Si es resta llama a resta y resta los númreos
+		if (currentSymbol == "-") {
+			return restar(valores[0], valores[1]);
+		}
+	}
+
+	// Actualiza los números en la memoria de la calcualdora
+	function actualizarDisplay(valorNuevo) {
+		// Ultimo valor en la memoria de la calculadora
+		const ultimoValor = displayList[displayList.length - 1];
+		// Revisar si la calculadora está en sus valores de inicio
+		const checkIfDefault = displayList[0] == 0 && displayList.length == 1;
+
+		// Cuando preciona el simbolo igual se ejecuta
 		if (valorNuevo == "=") {
+			// Si está en sus valores por efecto no cambia y devuelve 0
 			if (checkIfDefault) {
 				setdisplayList(() => displayList);
 				return;
 			}
-			const valores = encontrarValores();
-			if (currentSymbol == "+") {
-				setdisplayList(() => [sumar(valores[0], valores[1])]);
-				return;
-			}
-			if (currentSymbol == "-") {
-				setdisplayList(() => [restar(valores[0], valores[1])]);
-				return;
-			}
+			// Si ya hay una operación entonces hace la operación y la coloca 
+			// en la memoria
+			const resultado = [igual()];
+			setdisplayList(() => resultado);
+			return;
 		}
 
-		// Caso el valor es 0 y preciona alguna operación
+		// Cuando preciona un simbolo pero el valor de la memoria es 0
+		// Coloca en 0 la memoria
 		if (simbolosAUsar.includes(valorNuevo) && checkIfDefault) {
 			setdisplayList(() => [0]);
 			return;
 		}
 
-		// Caso preciona un número y luego una operación
+		// Revisa si el valor ingresado y el último en la memoria son simbolos
 		if (
 			!simbolosAUsar.includes(ultimoValor) ||
 			!simbolosAUsar.includes(valorNuevo)
 		) {
+			// Si tiene sus valores por defecto entonces el valor de la
+			// memoria será el nuevo número
 			if (checkIfDefault) {
 				setdisplayList(() => [valorNuevo]);
 				return;
 			}
+			// Si el último valor de la memoria es un número y
+			// el ingresado es un valor entonces define el simbolo
+			// de la operación y mete el símbolo a la memoria
 			if (simbolosAUsar.includes(valorNuevo)) {
 				setcurrentSymbol(valorNuevo);
-				setdisplayList(()=>[...displayList, valorNuevo])
+				setdisplayList(() => [...displayList, valorNuevo]);
 				return;
 			}
-			setdisplayList(()=>[...displayList, valorNuevo])
+			// Si es un número nuevo lo mete a la memoria
+			setdisplayList(() => [...displayList, valorNuevo]);
 			return;
 		}
-	}
-
-	function sumar(valor1, valor2) {
-		return valor1 + valor2;
-	}
-
-	function restar(valor1, valor2) {
-		return valor1 - valor2;
-	}
-
-	function dividir(valor1, valor2) {
-		console.log("dividiendo");
-	}
-
-	function multiplicar(valor1, valor2) {
-		console.log("multiplicando");
-	}
-
-	function igual() {
-		console.log("igual");
 	}
 
 	return (
